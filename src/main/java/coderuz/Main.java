4f8443f3df42b030148b31749447939f2160d57a
Coder-Kr <jasurbekOdilovn@mail.ru>
2025-09-04 16:51:52 +0900

    feat: spring-config.xml file qoshildi va uni ichida contactController bean yaratildi

--- src/main/java/coderuz/Main.java
+++ src/main/java/coderuz/Main.java
@@ -2,11 +2,14 @@ package coderuz;
 
 
 import coderuz.controller.ContactController;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
-        // lesson_11 (packages)
-        ContactController contactController = new ContactController();
+
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring-config.xml");
+        ContactController  contactController=context.getBean(ContactController.class);
         contactController.start();
     }
 }
