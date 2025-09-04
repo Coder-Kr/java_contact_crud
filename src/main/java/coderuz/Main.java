84ac00647ded5383ec68e827ab0aa548a72be230
Coder-Kr <jasurbekOdilovn@mail.ru>
2025-09-04 16:11:20 +0900

    feat: create contact manager with crud in java

---
+++ src/main/java/coderuz/Main.java
@@ -0,0 +1,12 @@
+package coderuz;
+
+
+import coderuz.controller.ContactController;
+
+public class Main {
+    public static void main(String[] args) {
+        // lesson_11 (packages)
+        ContactController contactController = new ContactController();
+        contactController.start();
+    }
+}
